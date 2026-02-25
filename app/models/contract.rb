1988395d451cd3c098850934d990996a418d6197
Alfred <kalumunabisimwa5@gmail.com>
2026-02-25 22:35:10 +0200

    Add model validations to Contract for data integrity

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -4,4 +4,9 @@ class Contract < ApplicationRecord
 
   # extracted_clauses: JSONB array of clause hashes
   # risk_flags: JSONB array of high/medium risk clause hashes
+  # Validations
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
+  validates :extracted_clauses, length: { maximum: 5000 }, allow_nil: true
+  validates :risk_flags, length: { maximum: 1000 }, allow_nil: true
 end
